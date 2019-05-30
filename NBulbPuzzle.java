987f37cb7a54af0b6faacc32311689bd7b79aa1e
dhulipala101 <51201599+dhulipala101@users.noreply.github.com>
2019-05-30 10:40:28 -0400

    Create NBulbPuzzle.java
    There are N bulbs, numbered 1 to N, arranged in a row. The first bulb is plugged into the power socket and each successive bulb is connected to the previous one(1-> 2->3...).
    Initially, all the bulbs are turned off. At moment K(for K from 0 to N-1), we turn on A[K]-th bulb. A bulb shines if it is on and all the previous bulbs are turned on too.
    
    Write a function solution that given an array A of N different integers from 1 to N, returns the number of moments for which every turned on bulb shines.

---
+++ NBulbPuzzle.java
@@ -0,0 +1,70 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+
+public class NBulbPuzzle {
+
+	public static int getTotalShiningMoments(int[] A)
+	{
+		int totalBulbs = A.length;
+		if (totalBulbs == 1 && A[0] == 1) return 1;
+		Map<Integer, Integer> stateDictionary = getInitializedStateDictionary(A);
+		int totalShiningMoments = 0;
+		for (int moment = 0; moment < A.length; moment ++)
+		{
+			if (stateDictionary.containsKey(A[moment])) {
+				stateDictionary.put(A[moment], 1);
+				totalShiningMoments += validateShiningMoment(moment, stateDictionary);
+			}
+			
+		}
+		return totalShiningMoments;
+ 	}
+	
+	public static int validateShiningMoment(int moment, Map<Integer, Integer> dictionary)
+	{
+		if (dictionary.containsKey(1) && dictionary.get(1) != 1)
+		{
+			return 0;
+		}
+		int i = 1;
+		for (; i <= moment; i++)
+		{
+		  if (dictionary.containsKey(i))
+		  {
+			  if (dictionary.get(i) == 1)
+			  {
+				  continue;
+			  }
+			  else return 0;
+		  }
+		}
+		return (i != 1 && dictionary.get(i-1) == 1)? 1: 0;
+	}
+	
+	public static Map<Integer, Integer> getInitializedStateDictionary(int[] A)
+	{
+		Map<Integer, Integer> dictionary = new HashMap<>();
+		IntStream intStream = Arrays.stream(A);
+		intStream.forEach(a -> dictionary.put(a, 0));
+		return dictionary;
+	}
+	
+	public static void main(String[] args)
+	{
+		// test input
+		int[] A = new int[] {2, 3, 1, 5, 4};
+		System.out.println("Total shining moments : " + getTotalShiningMoments(A));
+		
+		A = new int[] {1, 3, 4, 2, 5};
+		System.out.println("Total shining moments : " + getTotalShiningMoments(A));
+		
+		A = new int[] {2, 3, 4, 1, 5};
+		System.out.println("Total shining moments : " + getTotalShiningMoments(A));
+		
+		A = new int[] {1, 2, 3, 5, 4};
+		System.out.println("Total shining moments : " + getTotalShiningMoments(A));
+	}
+}
